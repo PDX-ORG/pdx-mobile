1690001f0eacb9a8651be3e4c9378853aa533090
Aleksey <alexey.yakovlef@gmail.com>
2023-01-21 13:49:25 +0700

    minor refactoring

--- sources/plugins/eve/src/main/kotlin/io/github/lexadiky/pdx/plugin/eve/EvePlugin.kt
+++ sources/plugins/eve/src/main/kotlin/io/github/lexadiky/pdx/plugin/eve/EvePlugin.kt
@@ -2,10 +2,8 @@ package io.github.lexadiky.pdx.plugin.eve
 
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-import org.gradle.kotlin.dsl.configure
 import org.gradle.kotlin.dsl.findByType
 import org.jetbrains.kotlin.gradle.dsl.KotlinProjectExtension
-import org.jetbrains.kotlin.gradle.model.KotlinAndroidExtension
 import java.io.File
 
 class EvePlugin : Plugin<Project> {
@@ -14,22 +12,23 @@ class EvePlugin : Plugin<Project> {
         val inputFile = target.objects.fileProperty()
         inputFile.set(File(target.projectDir, "analytics"))
 
-        val outputFile = target.objects.fileProperty()
-        outputFile.set(File(target.buildDir, "generated/sources/main/kotlin/eve"))
+        val outputFileProperty = target.objects.fileProperty()
+        val outputFile = File(target.buildDir, "generated/sources/main/kotlin/eve")
+        outputFileProperty.set(outputFile)
 
         target.tasks.register("eveBuild", EveBuildTask::class.java) {
             input.set(inputFile)
-            output.set(outputFile)
+            output.set(outputFileProperty)
         }
         target.tasks.findByName("preBuild")
             ?.dependsOn("eveBuild")
 
         target.extensions.findByType<KotlinProjectExtension>()!!.apply {
             sourceSets.getByName("main") {
-                kotlin.srcDir("build/generated/sources/main/kotlin/eve")
+                kotlin.srcDir(outputFile)
             }
             sourceSets.getByName("test") {
-                kotlin.srcDir("build/generated/sources/main/kotlin/eve")
+                kotlin.srcDir(outputFile)
             }
         }
     }
