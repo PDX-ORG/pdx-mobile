d138beae352fb47a9b81614a2f67b9c60fb3092c
Aleksey <alexey.yakovlef@gmail.com>
2023-02-02 19:17:05 +0700

    - added a bit more achievements

--- sources/domain/achievement/src/main/java/io/github/lexadiky/pdx/domain/achievement/AchievementLibraryFactory.kt
+++ sources/domain/achievement/src/main/java/io/github/lexadiky/pdx/domain/achievement/AchievementLibraryFactory.kt
@@ -3,11 +3,15 @@ package io.github.lexadiky.pdx.domain.achievement
 import io.github.lexadiky.pdx.domain.achievement.library.Achievement
 import io.github.lexadiky.pdx.domain.achievement.library.ShinyShakeAchievement
 import io.github.lexadiky.pdx.domain.achievement.library.WhoIsBeginnerAchievement
+import io.github.lexadiky.pdx.domain.achievement.library.WhoIsChampionAchievement
+import io.github.lexadiky.pdx.domain.achievement.library.WhoIsTrainerAchievement
 
 class AchievementLibraryFactory {
 
     fun list(): List<Achievement> = listOf(
         ShinyShakeAchievement(),
-        WhoIsBeginnerAchievement()
+        WhoIsBeginnerAchievement(),
+        WhoIsTrainerAchievement(),
+        WhoIsChampionAchievement()
     )
 }
