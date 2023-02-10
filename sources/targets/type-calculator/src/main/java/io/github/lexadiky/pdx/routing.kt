71aee1224b75c0c367d87dfd8f82b0839fc44d37
Aleksey <alexey.yakovlef@gmail.com>
2023-02-10 18:27:28 +0700

    fixed github build, try 6

--- sources/targets/type-calculator/src/main/java/io/github/lexadiky/pdx/routing.kt
+++ sources/targets/type-calculator/src/main/java/io/github/lexadiky/pdx/routing.kt
@@ -3,6 +3,7 @@ package io.github.lexadiky.pdx
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import io.github.lexadiky.akore.alice.robo.di
+import io.github.lexadiky.akore.alice.robo.inject
 import io.github.lexadiky.pdx.feature.settings.SettingsPage
 import io.github.lexadiky.pdx.feature.settings.achievement.AchievementSettingsPage
 import io.github.lexadiky.pdx.feature.typechart.TypePage
