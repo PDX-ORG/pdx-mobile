8d7adca2a8be210b0c874a2fd6330316fd5aa6d6
Aleksey <alexey.yakovlef@gmail.com>
2023-03-11 14:55:00 +0700

    remade featured pokemon logic

--- sources/domain/pokemon/src/main/java/io/github/lexadiky/pdx/domain/pokemon/usecase/GetPokemonPreviewSampleUseCase.kt
+++ sources/domain/pokemon/src/main/java/io/github/lexadiky/pdx/domain/pokemon/usecase/GetPokemonPreviewSampleUseCase.kt
@@ -6,8 +6,12 @@ import io.github.lexadiky.pdx.domain.pokemon.entity.PokemonPreview
 import io.github.lexadiky.pdx.domain.pokemon.entity.PokemonType
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
+import kotlinx.datetime.Clock
+import kotlin.random.Random
 
 class GetPokemonPreviewSampleUseCase(private val getPokemonPreview: GetPokemonPreviewUseCase) {
+
+    private val randomGenerator = Random(Clock.System.now().epochSeconds / (60 * 60 * 24))
 
     suspend operator fun invoke(size: Int): Either<Error, List<PokemonPreview>> = withContext(Dispatchers.IO) {
         getPokemonPreview.invoke()
@@ -16,9 +20,9 @@ class GetPokemonPreviewSampleUseCase(private val getPokemonPreview: GetPokemonPr
     }
 
     private fun takeSample(pokemonPreviews: List<PokemonPreview>, size: Int): List<PokemonPreview> {
-        val anchorType = PokemonType.values().random()
+        val anchorType = PokemonType.values().random(randomGenerator)
         return pokemonPreviews.filter { anchorType in it.types }
-            .shuffled()
+            .shuffled(randomGenerator)
             .take(size)
     }
 
