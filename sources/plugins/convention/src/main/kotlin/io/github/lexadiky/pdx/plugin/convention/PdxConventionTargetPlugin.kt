fce58740ea75807ee5af24a360617a4e83fd300f
Aleksey <alexey.yakovlef@gmail.com>
2023-03-31 18:56:31 +0700

    - version bump

--- sources/plugins/convention/src/main/kotlin/io/github/lexadiky/pdx/plugin/convention/PdxConventionTargetPlugin.kt
+++ sources/plugins/convention/src/main/kotlin/io/github/lexadiky/pdx/plugin/convention/PdxConventionTargetPlugin.kt
@@ -25,8 +25,8 @@ class PdxConventionTargetPlugin : Plugin<Project> {
         defaultConfig {
             minSdk = 24
             targetSdk = 33
-            versionCode = 1
-            versionName = "1.0"
+            versionCode = 2
+            versionName = "0.1.0"
 
             testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
             vectorDrawables {
