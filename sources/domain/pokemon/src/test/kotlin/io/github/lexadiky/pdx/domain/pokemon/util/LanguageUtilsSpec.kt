50986ceeadbe82c48e5c8f42af447b51f5e0867e
Aleksey <alexey.yakovlef@gmail.com>
2023-05-18 22:04:33 +0700

    - tests for language utils

--- sources/domain/pokemon/src/test/kotlin/io/github/lexadiky/pdx/domain/pokemon/util/LanguageUtilsSpec.kt
+++ sources/domain/pokemon/src/test/kotlin/io/github/lexadiky/pdx/domain/pokemon/util/LanguageUtilsSpec.kt
@@ -1,22 +1,59 @@
 package io.github.lexadiky.pdx.domain.pokemon.util
 
 import io.github.lexadiky.pdx.domain.pokemon.entity.PokemonLanguage
+import io.github.lexadiky.pdx.lib.locale.LocaleManager
+import io.github.lexadiky.pdx.lib.locale.LocaleSettings
 import io.kotest.core.spec.style.DescribeSpec
 import io.kotest.inspectors.forAll
 import io.kotest.matchers.equals.shouldBeEqual
+import io.lexadiky.pokeapi.entity.common.Name
+import io.lexadiky.pokeapi.entity.common.ResourcePointer
+import io.mockk.every
+import io.mockk.mockk
 import java.util.Locale
 
 class LanguageUtilsSpec : DescribeSpec({
     describe("asPokemonLanguage") {
         describe("via backward conversion") {
-            describe("known languages") {
-                PokemonLanguage.values().forAll { language ->
-                    it("language '${language.languageTag}' should be convertible") {
-                        val locale = Locale.forLanguageTag(language.languageTag)
-                        locale.asPokemonLanguage() shouldBeEqual language
-                    }
+            PokemonLanguage.values().forAll { language ->
+                it("language '${language.languageTag}' should be convertible") {
+                    val locale = Locale.forLanguageTag(language.languageTag)
+                    locale.asPokemonLanguage() shouldBeEqual language
                 }
             }
+        }
+        it("unknown language") {
+            Locale.forLanguageTag("th").asPokemonLanguage() shouldBeEqual PokemonLanguage.default()
+        }
+    }
+
+    describe("ofCurrentLocale") {
+        val localeManager = mockk<LocaleManager> {
+            every { settings } returns LocaleSettings(
+                system = Locale.ENGLISH,
+                flags = emptyList()
+            )
+        }
+
+        it("given empty list of names should return empty string") {
+            emptyList<Name>().ofCurrentLocale(localeManager) shouldBeEqual ""
+        }
+
+        it("given list of names not containing matching locale return first available") {
+            val names = listOf(
+                Name("hello world", ResourcePointer("ja", "")),
+                Name("hello world in zh", ResourcePointer("ko", ""))
+            )
+            names.ofCurrentLocale(localeManager) shouldBeEqual "hello world"
+        }
+
+        it("given list containing matching locale return matching string") {
+            val names = listOf(
+                Name("hello world in ja", ResourcePointer("ja", "")),
+                Name("hello world", ResourcePointer("en", ""))
+            )
+
+            names.ofCurrentLocale(localeManager) shouldBeEqual "hello world"
         }
     }
 })
